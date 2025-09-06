bc0f7c133764d15e45294598d08fee9f52460405
Zammil00 <muzammilkw07@gmail.com>
2025-09-06 12:38:28 +0700

    Create Page Auth

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,15 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const REGISTER = _Paths.REGISTER;
+  static const LOGIN = _Paths.LOGIN;
+  static const SPLASH = _Paths.SPLASH;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const REGISTER = '/register';
+  static const LOGIN = '/login';
+  static const SPLASH = '/splash';
 }
