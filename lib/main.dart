f9f56a0e44ac0afca2cc0507ae1bec742666db23
Zammil00 <muzammilkw07@gmail.com>
2025-09-07 19:01:39 +0700

    Init Supabase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // INIT DOTENV FOR ENVIRONMENT VARIABLES
+  await dotenv.load(fileName: ".env");
+
+  Supabase supabase = await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? "",
+    anonKey: dotenv.env['SUPABASE_ANNON_KEY'] ?? "",
+  );
+
+  if (kDebugMode) {
+    print("SUPA KONEK GA ? = ${supabase.client.auth.currentSession}");
+  }
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
